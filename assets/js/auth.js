6a47137868b115ede600231fd338aeb2fb348463
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 09:06:19 +0700

    Update auth.js

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,6 +1,11 @@
 window.SFMS_AUTH = {
   async login(username, password) {
     const res = await window.SFMS_API.login(username, password);
+
+    if (!res || !res.session) {
+      throw new Error("Response login tidak memiliki session");
+    }
+
     window.SFMS_STATE.session = res.session;
     window.SFMS_UTILS.saveSession(res.session);
     return res.session;
@@ -8,6 +13,11 @@ window.SFMS_AUTH = {
 
   async bootstrap() {
     const session = window.SFMS_STATE.session;
+
+    if (!session) {
+      throw new Error("Session belum tersedia");
+    }
+
     const res = await window.SFMS_API.bootstrap(session);
     window.SFMS_STATE.db = res.data;
     return res.data;
