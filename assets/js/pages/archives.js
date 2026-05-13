60390cb16ae33ec2da06f748f2bced3745b5d4ee
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 08:37:59 +0700

    Create archives.js

---
+++ assets/js/pages/archives.js
@@ -0,0 +1,8 @@
+window.SFMS_PAGES.archives = function () {
+  document.getElementById("page").innerHTML = `
+    <div class="card">
+      <h2>Arsip</h2>
+      <div class="empty">Modul arsip akan kita isi pada tahap berikutnya.</div>
+    </div>
+  `;
+};
