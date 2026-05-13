ec1056fac9b1c8474e6932c77d3ff02bf5150d30
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 07:14:50 +0700

    Create scanner.js

---
+++ assets/js/pages/scanner.js
@@ -0,0 +1,8 @@
+window.SFMS_PAGES.scanner = function () {
+  document.getElementById("page").innerHTML = `
+    <div class="card">
+      <h2>Scanner</h2>
+      <div class="empty">Modul scanner akan kita isi pada tahap berikutnya.</div>
+    </div>
+  `;
+};
