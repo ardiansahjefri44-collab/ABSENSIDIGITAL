46a5fe0b22be104cdbc46516ed48aa03b5a96635
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 08:38:39 +0700

    Create settings.js

---
+++ assets/js/pages/settings.js
@@ -0,0 +1,8 @@
+window.SFMS_PAGES.settings = function () {
+  document.getElementById("page").innerHTML = `
+    <div class="card">
+      <h2>Pengaturan</h2>
+      <div class="empty">Modul pengaturan akan kita isi pada tahap berikutnya.</div>
+    </div>
+  `;
+};
