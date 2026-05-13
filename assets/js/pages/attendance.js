765aa5fa1c3bd234c4fad9cf187b3c6d70e409a6
ardiansahjefri44-collab <ardiansahjefri44@gmail.com>
2026-05-13 07:14:17 +0700

    Create attendance.js

---
+++ assets/js/pages/attendance.js
@@ -0,0 +1,8 @@
+window.SFMS_PAGES.attendance = function () {
+  document.getElementById("page").innerHTML = `
+    <div class="card">
+      <h2>Absensi Siswa</h2>
+      <div class="empty">Modul absensi siswa akan kita isi pada tahap berikutnya.</div>
+    </div>
+  `;
+};
